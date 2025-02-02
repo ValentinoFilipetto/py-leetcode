a33e21ff925245d163318af753bdd4f4152b9fe9
ValentinoFilipetto <valefilipetto@gmail.com>
2025-02-02 19:38:07 +0100

    add valid palindrome and kth smallest element in BST

---
+++ Easy/ValidPalindrome.py
@@ -0,0 +1,19 @@
+# Time complexity: O(n)
+# Space complexity: O(n)
+
+
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        alphanumeric = "abcdefghijklmnopqrstuvwxyz0123456789"
+        cleaned_string = "".join([char for char in s.lower() if char in alphanumeric])
+
+        if len(cleaned_string) == 0:
+            return True
+
+        reversed_cleaned_string = cleaned_string[::-1]
+
+        for j in range(len(cleaned_string)):
+            if cleaned_string[j] != reversed_cleaned_string[j]:
+                return False
+
+        return True
