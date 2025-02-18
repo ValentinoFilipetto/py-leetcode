036e3034ff127449024d1d10b8844f4dcb67f4d2
ValentinoFilipetto <valefilipetto@gmail.com>
2025-02-18 17:48:39 +0100

    add remove duplicates from linked list

---
+++ Easy/RemoveDuplicatesFromSortedList.py
@@ -0,0 +1,47 @@
+from typing import Optional
+
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+# Time complexity: O(n)
+# Space complexity: O(n)
+
+
+class LinearSpaceSolution:
+    def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if not head:
+            return None
+
+        current = ListNode(head.val)
+        res_head = current
+
+        while head.next is not None:
+            if head.next.val != head.val:
+                current.next = ListNode(head.next.val)
+                current = current.next
+
+            head = head.next
+
+        return res_head
+
+
+# Time complexity: O(n)
+# Space complexity: O(1)
+
+
+class ConstantSpaceSolution:
+    def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        curr = head
+
+        while curr:
+            while curr.next and curr.val == curr.next.val:
+                curr.next = curr.next.next
+
+            curr = curr.next
+
+        return head
