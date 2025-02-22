b219e9ab3b522ad9eca30786d9c2d33b45924ca6
ValentinoFilipetto <valefilipetto@gmail.com>
2025-02-22 12:45:27 +0100

    add first unique character in a string

---
+++ Easy/FirstUniqueChar.py
@@ -0,0 +1,20 @@
+from collections import defaultdict
+
+# Time complexity: O(n)
+# Space complexity: O(n)
+
+
+class Solution:
+    def firstUniqChar(self, s: str) -> int:
+        count = defaultdict(
+            int
+        )  # this allows us to do count[letter] += 1, without checking for the existence of letter
+
+        for letter in s:
+            count[letter] += 1
+
+        for i, letter in enumerate(s):
+            if count[letter] == 1:
+                return i
+
+        return -1
