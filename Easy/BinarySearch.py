a5383fd8ccb369faabae512b966765f590c76f9b
ValentinoFilipetto <valefilipetto@gmail.com>
2025-02-22 11:15:39 +0100

    add binary search

---
+++ Easy/BinarySearch.py
@@ -0,0 +1,21 @@
+from typing import List
+
+# Time complexity: n(log n)
+# Space complexity: O(1)
+
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        l, r = 0, len(nums) - 1
+
+        while l <= r:
+            m = l + ((r - l) // 2)
+
+            if target < nums[m]:
+                r = m - 1
+            elif target > nums[m]:
+                l = m + 1
+            else:
+                return m
+
+        return -1
