38e2b5228f6be0681b563ef2ab4ce5cb86919f10
ValentinoFilipetto <valefilipetto@gmail.com>
2025-02-23 20:03:54 +0100

    add two solutions for intersection of two linked lists

---
+++ Easy/IntersectionOfTwoLinkedLists.py
@@ -0,0 +1,44 @@
+from collections import defaultdict
+from typing import Optional
+from DataStructures import ListNode
+
+# Time complexity: O(m + n)
+# Space complexity: O(m + n)
+
+
+class HashMapSolution:
+    def getIntersectionNode(
+        self, headA: ListNode, headB: ListNode
+    ) -> Optional[ListNode]:
+        map = defaultdict(int)
+
+        while headA:
+            map[headA] += 1
+            headA = headA.next
+
+        while headB:
+            map[headB] += 1
+            headB = headB.next
+
+        for node in map:
+            if map[node] == 2:
+                return node
+
+        return None
+
+
+# Time complexity: O(m + n)
+# Space complexity: O(1)
+
+
+class ConstantMemorySolution:
+    def getIntersectionNode(
+        self, headA: ListNode, headB: ListNode
+    ) -> Optional[ListNode]:
+        l1, l2 = headA, headB
+
+        while l1 != l2:
+            l1 = l1.next if l1 else headB
+            l2 = l2.next if l2 else headA
+
+        return l1
