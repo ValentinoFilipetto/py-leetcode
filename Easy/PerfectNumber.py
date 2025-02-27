da0cd421dbc844afd9440496193460d4a3108736
ValentinoFilipetto <valefilipetto@gmail.com>
2025-02-27 08:30:57 +0100

    add perfect number

---
+++ Easy/PerfectNumber.py
@@ -0,0 +1,16 @@
+import math
+
+# Time complexity: O(sqrt(n))
+
+
+class Solution:
+    def checkPerfectNumber(self, num: int) -> bool:
+        if num < 5:
+            return False
+
+        sum = 1
+        for i in range(2, int(math.sqrt(num)) + 1):
+            if num % i == 0:
+                sum += i + num // i
+
+        return sum == num
