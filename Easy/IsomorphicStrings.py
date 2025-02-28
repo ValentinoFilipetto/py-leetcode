26131cfd32e2ff2db0f451d35124c9f3912abb08
ValentinoFilipetto <valefilipetto@gmail.com>
2025-02-28 09:35:15 +0100

    add Isomorphic Strings solutions

---
+++ Easy/IsomorphicStrings.py
@@ -0,0 +1,22 @@
+from collections import defaultdict
+
+# Time complexity: O(n)
+# Space complexity: O(n)
+
+
+class Solution:
+    def isIsomorphic(self, s: str, t: str) -> bool:
+        hashMap = defaultdict(str)
+        res = ""
+        
+        for i in range(0, len(s)):
+            c1, c2 = s[i], t[i]
+
+            if c2 in hashMap.values():
+                continue
+            hashMap[c1] = c2
+
+        for i in range(0, len(s)):
+            res += hashMap[s[i]]
+
+        return res == t
