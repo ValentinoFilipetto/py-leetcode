291da3bb19f6d5cd1f4084077d02af7d7ae867d1
ValentinoFilipetto <valefilipetto@gmail.com>
2025-03-04 09:10:43 +0100

    add palindrome linked liste

---
+++ Easy/PalindromeLinkedList.py
@@ -0,0 +1,22 @@
+from typing import Optional
+from DataStructures import ListNode
+
+# Time complexity: O(n)
+# Space complexity: O(n)
+
+class Solution:
+    def isPalindrome(self, head: Optional[ListNode]) -> bool:
+        res = []
+
+        if not head:
+            return False
+
+        while head:
+            res.append(head.val)
+            head = head.next
+
+        for i in range(len(res) // 2):
+            if res[i] != res[len(res) - 1 - i]:
+                return False
+
+        return True
