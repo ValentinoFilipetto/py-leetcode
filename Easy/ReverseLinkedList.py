39533abbd14ac6d8be4955e80b65d2f9806c201a
ValentinoFilipetto <valefilipetto@gmail.com>
2025-03-05 09:24:21 +0100

    add reverse linked list

---
+++ Easy/ReverseLinkedList.py
@@ -0,0 +1,41 @@
+from typing import Optional
+from DataStructures import ListNode
+
+# Time complexity: O(n)
+# Space complexity O(n)
+
+
+class LinearSpaceSolution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        array_of_nodes = []
+        my_linked_list = ListNode(0)
+        res = my_linked_list
+
+        while head:
+            array_of_nodes.append(head.val)
+            head = head.next
+
+        reversedArray = array_of_nodes[::-1]
+
+        for val in reversedArray:
+            my_linked_list.next = ListNode(val)
+            my_linked_list = my_linked_list.next
+
+        return res.next
+    
+# Time complexity: O(n)
+# Space complexity O(1)
+    
+class ConstantSpaceSolution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:  
+        prev, curr = None, head
+        
+        while curr:
+            tmp = curr.next
+            curr.next = prev
+            prev = curr
+            curr = tmp
+        
+        return prev
+    
+# Recursive solution: to be added
