d63ec0f09fb1ced73717f41883a243b61df11b34
M67E863 <ae09vf@insim.biz>
2025-05-12 09:50:30 +0200

    add intuition for MoveZeroes

--- Easy/MoveZeroes.py
+++ Easy/MoveZeroes.py
@@ -1,3 +1,4 @@
+# Intuition: Instead of moving 0's to the end of the array, we move positive numbers to the front.
 # Patter: Two Pointers
 # Time complexity: O(n)
 # Space complexity: O(1), as we change the array "in place"
