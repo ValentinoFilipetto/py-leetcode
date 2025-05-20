dbf27d966f291397ec82fecd28c7de35d96fe3c5
M67E863 <ae09vf@insim.biz>
2025-05-20 09:46:53 +0200

    add ramson note

---
+++ Easy/RansomNote.py
@@ -0,0 +1,21 @@
+from collections import defaultdict
+
+# Pattern: Hash Map
+# Time complexity: O(n + m)
+# Space complexity: O(k), where k is the number of unique characters in magazine,
+# (at most 26 for lowercase English letters, so effectively O(1) for English alphabet, but O(k) in general).
+
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        my_map = defaultdict(int)
+
+        for letter in magazine:
+            my_map[letter] += 1
+
+        for letter in ransomNote:
+            if letter in my_map and my_map[letter]:
+                my_map[letter] -= 1
+            else:
+                return False
+
+        return True
