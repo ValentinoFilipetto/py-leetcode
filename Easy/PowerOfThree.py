61a22e4322f492f8900e8e3dbd688efb41589c40
M67E863 <ae09vf@insim.biz>
2025-05-20 09:33:17 +0200

    add power of three

---
+++ Easy/PowerOfThree.py
@@ -0,0 +1,12 @@
+# Pattern: Iteration
+# Time complexity: O(log n)
+# Space complexity: O(1)
+
+class Solution:
+    def isPowerOfThree(self, n: int) -> bool:
+        # if the number is less than 1, it cannot be a power of 3
+        if n == 1:
+            return True
+        while n > 3:
+            n /= 3
+        return (n == 3)
