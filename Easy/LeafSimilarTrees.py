0e0eb8d74cfcba855d97ee00f9d2bc2e48a3dde2
ValentinoFilipetto <valefilipetto@gmail.com>
2025-05-28 09:15:15 +0200

    add leaf similar trees

---
+++ Easy/LeafSimilarTrees.py
@@ -0,0 +1,24 @@
+from typing import Optional
+from DataStructures import TreeNode
+
+# Pattern: DFS
+# Time complexity: O(n + m)
+# Space complexity: O(m + n)
+
+class Solution:
+    def leafSimilar(self, root1: Optional[TreeNode], root2: Optional[TreeNode]) -> bool:
+        leaf_sequence1, leaf_sequence2 = [], []
+
+        def dfs(node, arr):
+            if not node:
+                return
+            if not node.left and not node.right:
+                arr.append(node.val)
+
+            dfs(node.left, arr)
+            dfs(node.right, arr)
+
+        dfs(root1, leaf_sequence1)
+        dfs(root2, leaf_sequence2)
+
+        return leaf_sequence1 == leaf_sequence2
